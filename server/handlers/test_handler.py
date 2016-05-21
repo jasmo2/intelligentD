e9c9b56a2b00b5d1b141ac17f77e8cd601ebbd9b
lef <le.florez602@uniandes.edu.co>
2016-05-21 15:18:10 -0500

    download csv

--- server/handlers/test_handler.py
+++ server/handlers/test_handler.py
@@ -7,6 +7,7 @@ from tornado.options import options
 from datamining.datamining import analyse, executeModel
 import numpy as np
 import csv
+import pandas
 
 class TestHandler(web.RequestHandler):
     def get(self):
@@ -23,11 +24,20 @@ class TestHandler(web.RequestHandler):
         prediction = executeModel(res['modelo'], X[4:5])
         # self.render("index_path")
 
+        print(prediction)
+        df = pandas.DataFrame(data=prediction, columns=['a'])
+        df.to_csv('eggs.csv')
+        # print(str(type(prediction)))
 
-        with open('eggs.csv', 'w', newline='') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=',',
-                                    quoting=csv.QUOTE_MINIMAL)
-            spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-            spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+        # with open('eggs.csv', 'w', newline='') as csvfile:
+        #     spamwriter = csv.writer(csvfile, delimiter=',',
+        #                             quoting=csv.QUOTE_MINIMAL)
+        #     spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+        #     spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
 
-        self.write(str(res['cossvalidation']) + str(prediction))
+        # self.write(str(res['cossvalidation']) + str(prediction))
+
+        ifile  = open("eggs.csv", "r")
+        self.set_header ('Content-Type', 'text/csv')
+        self.set_header ('Content-Disposition', 'attachment; filename='+"eggs.csv"+'')
+        self.write (ifile.read())
